bfd8c30d44147940a64a7060712a8f1228a1904e
seanspencr <sean280806@gmail.com>
2025-08-03 13:54:12 +0700

    benerin inventory

--- commands.sh
+++ commands.sh
@@ -1,1 +1,8 @@
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.ini playbook-create-k8s-cluster.yml --tags "install-cni" -u demo --private-key=$HOME/id_rsa 
+
+
+# Run Flannel fix
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.ini playbook-create-k8s-cluster.yml --tags "fix-flannel" -u demo --private-key=$HOME/id_rsa
+
+# Run containerd fix
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.ini playbook-create-k8s-cluster.yml --tags "fix-containerd" -u demo --private-key=$HOME/id_rsa
